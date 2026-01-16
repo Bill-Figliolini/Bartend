fd262abe8e13de437979f3290c6e9f20c96883a9
Bill Figliolini <BillFigliolini@gmail.com>
2026-01-16 15:20:56 -0500

    initial iced test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    Element,
+    widget::{button, text},
+};
+
+#[derive(Debug, Clone)]
+enum Message {
+    Increment,
+}
+
+fn main() -> iced::Result {
+    iced::run(update, view)
+}
+fn update(counter: &mut u64, message: Message) {
+    match message {
+        Message::Increment => *counter += 1,
+    }
+}
+
+fn view(counter: &u64) -> Element<'_, Message> {
+    button(text(counter)).on_press(Message::Increment).into()
 }
