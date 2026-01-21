1fedb0ec52195ebecba214a956373fe014ded365
Bill Figliolini <BillFigliolini@gmail.com>
2026-01-21 17:44:35 -0500

    Made Item into the more descriptive button

--- src/presentation/widget/sidebar.rs
+++ src/presentation/widget/sidebar.rs
@@ -1,10 +1,10 @@
-use iced::{Element, Length::Fill, widget::button};
+use iced::{Element, Length::Fill, widget};
 
-pub fn item<'a, Message: Clone + 'a>(
+pub fn button<'a, Message: Clone + 'a>(
     content: impl Into<Element<'a, Message>>,
     on_press: impl Fn() -> Message + 'a,
 ) -> Element<'a, Message> {
-    button(content)
+    widget::button(content)
         .on_press_with(on_press)
         .padding(10)
         .width(Fill)
