d53a042e8da98ab81a8cbb69939d29a9a97a1f8d
Bill Figliolini <BillFigliolini@gmail.com>
2026-01-21 17:45:03 -0500

    Cleaned up screen module

--- src/presentation/screen/mod.rs
+++ src/presentation/screen/mod.rs
@@ -1,5 +1,5 @@
-use iced::overlay::Element;
-
 //Screens are self contained and will contain their own state. Transitions between them, as of current design,
 // Will be handled by the main application.
+pub(super) mod inventory;
 pub(super) mod overview;
+pub(super) mod recipe;
