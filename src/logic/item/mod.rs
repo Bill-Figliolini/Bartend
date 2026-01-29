5c90ca128412fffbfbfb1ab674bc4122f90258bf
Bill Figliolini <BillFigliolini@gmail.com>
2026-01-29 18:16:00 -0500

    Basis for Item Handling. To be completed once ID Table is Implemented.

---
+++ src/logic/item/mod.rs
@@ -0,0 +1,32 @@
+//! Module for handling user provided ingredients and items.
+//!
+//! Each Item is Composed of a Name and a Quantity
+//! Each Item is Stored inside and owned by Items, a HashTable
+//! Items are given unique IDs, so that they can be edited.
+//!
+
+use fnv::FnvHashMap;
+
+use crate::logic::{Error, Quantity, common::id_table::IdTable};
+
+pub(super) struct Items {
+    table: IdTable<ItemID, Item>,
+}
+
+struct ItemID(u32);
+
+struct Item {}
+
+impl Items {
+    fn new() -> Self {
+        Self {
+            table: IdTable::new(ItemID),
+        }
+    }
+    fn insert(&mut self, name: String, quantity: Quantity) {}
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+}
