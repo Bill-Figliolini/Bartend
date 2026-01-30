856a81e04fbdcb20b18b43e2eaf7eef6a07ef991
Bill Figliolini <BillFigliolini@gmail.com>
2026-01-30 17:44:26 -0500

    Removed uom references

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -2,15 +2,13 @@ mod common;
 mod item;
 use std::ops::Add;
 
-use uom::si::{mass::gram, volume::milliliter};
-
 /// Defines potential manners in which the quantity of an ingredient can be defined.
 /// Mass and Volume are handled by uom measures
 /// Count is an i32 that can be multiplied into a float and interpreted by the user
 pub enum Quantity {
-    Mass(gram),
-    Volume(milliliter),
-    Count(i32),
+    Mass(u32),
+    Volume(u32),
+    Count(u32),
 }
 
 enum Error {
