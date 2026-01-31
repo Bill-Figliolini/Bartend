b20431c46f93997bc3ffecdd73e8643c4ae9a185
Bill Figliolini <BillFigliolini@gmail.com>
2026-01-30 19:16:34 -0500

    Basic implementation of Items Logic

--- src/logic/item/mod.rs
+++ src/logic/item/mod.rs
@@ -9,16 +9,22 @@ use fnv::FnvHashMap;
 
 use crate::logic::{Error, Quantity, common::id_table::IdTable};
 
+struct Item {
+    name: String,
+    quantity: u32,
+}
+
+impl Item {
+    fn new(name: String, quantity: u32) -> Item {
+        Item { name, quantity }
+    }
+}
+
 pub(super) struct Items {
     table: IdTable<ItemID, Item>,
 }
 #[derive(Hash, PartialEq, Eq, Clone, Copy)]
-struct ItemID(u32);
-
-struct Item {
-    name: String,
-    volume: u32,
-}
+pub struct ItemID(u32);
 
 impl Items {
     fn new() -> Self {
@@ -26,10 +32,42 @@ impl Items {
             table: IdTable::new(ItemID),
         }
     }
-    fn insert(&mut self, name: String, quantity: Quantity) {}
+    fn insert(&mut self, name: String, quantity: u32) -> ItemID {
+        let item = Item::new(name, quantity);
+        self.table.insert(item)
+    }
+    fn delete(&mut self, id: ItemID) {
+        self.table.remove(id);
+    }
+    fn get(&self, id: &ItemID) -> &Item {
+        self.table.get(id)
+    }
+    fn update_quantities<const N: usize>(&mut self, ids: [&ItemID; N], quantities: [&u32; N]) {
+        let values = self.table.get_disjoint_mut(ids);
+        for i in (0..N) {
+            values[i].quantity -= quantities[i];
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    #[test]
+    fn batch_updates_table() {
+        let mut items = Items::new();
+        let mut ids = vec![];
+        for char in "abc".chars() {
+            let index = items.insert(char.to_string(), 750);
+            ids.push(index);
+        }
+        let changed_ids = [&ids[0], &ids[2]];
+        let changed_quantities = [&200, &100];
+
+        items.update_quantities(changed_ids, changed_quantities);
+
+        assert_eq!(items.get(&ids[0]).quantity, 550);
+        assert_eq!(items.get(&ids[1]).quantity, 750);
+        assert_eq!(items.get(&ids[2]).quantity, 650);
+    }
 }
