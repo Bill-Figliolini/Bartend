61dbabc29cafcc5d5136cba1dc33d472cd63503b
Bill Figliolini <BillFigliolini@gmail.com>
2026-02-06 19:11:13 -0500

    Removed SchemaBUilder

--- src/persistence/sqlite/schema.rs
+++ src/persistence/sqlite/schema.rs
@@ -7,33 +7,22 @@ pub(super) struct SchemaBuilder {
     name: String,
     columns: Vec<String>,
 }
-
-impl SchemaBuilder {
-    pub fn column(mut self, column: String) -> Self {
-        self.columns.push(column);
-        Self {
-            name: self.name,
-            columns: self.columns,
-        }
-    }
-
-    pub fn build(self) -> Schema {
-        Schema {
-            name: self.name,
-            columns: self.columns,
-        }
-    }
-}
-
 impl Schema {
-    pub fn new(name: &str) -> SchemaBuilder {
-        SchemaBuilder {
+    pub fn new(name: &str) -> Self {
+        Self {
             name: name.to_string(),
             columns: vec![],
         }
     }
     pub fn name(&self) -> &str {
         &self.name
+    }
+    pub fn column(mut self, column: &str) -> Self {
+        self.columns.push(column.to_string());
+        Self {
+            name: self.name,
+            columns: self.columns,
+        }
     }
     pub fn columns(&self) -> &Vec<String> {
         &self.columns
