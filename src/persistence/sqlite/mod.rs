3d8ec8a83ac540355d667bcab90ab570336e11eb
Bill Figliolini <BillFigliolini@gmail.com>
2026-02-06 19:11:01 -0500

    Added Sqlite tests

--- src/persistence/sqlite/mod.rs
+++ src/persistence/sqlite/mod.rs
@@ -2,55 +2,69 @@ mod schema;
 
 use std::{fmt::Display, path::Path};
 
-use crate::persistence::{PersistenceError, sqlite::schema::Schema};
+use crate::persistence::{PersistenceError, Repository, sqlite::schema::Schema};
 use rusqlite::{self, Connection};
 use sql_query_builder::{self, CreateTable, Insert};
 
 struct DB {
-    db: Connection,
+    connection: Connection,
 }
 impl DB {
-    fn new() -> Result<Self, PersistenceError> {
-        todo!()
+    fn new(path: impl AsRef<Path>) -> Self {
+        let connection = Connection::open(path);
+        match connection {
+            Ok(connection) => Self { connection },
+            Err(e) => {
+                panic!("DB could not be opened! {}", e)
+            }
+        }
     }
     fn get(&self) {
         todo!()
     }
 }
 
+impl Repository for DB {}
+
 //There's a design decision here that I had not considered.
 // I could formulate each table as a struct of its names and fields, and use that to handle creation and insertion,
 // instead of doing it manually.
-fn create_tables(db: &DB) -> Result<(), PersistenceError> {
+fn create_tables(db: &DB) {
     //Initialize the Items Tables
 
     let create_items = CreateTable::new()
         .create_table_if_not_exists("items")
         .column("id INTEGER PRIMARY KEY")
         .column("name TEXT NOT NULL")
-        .as_string();
-    db.db.execute(&create_items, ());
-
-    let create_item_quantities = CreateTable::new()
-        .create_table_if_not_exists("item_quantities")
-        .column("id INTEGER PRIMARY KEY")
         .column("quantity REAL NOT NULL")
-        .foreign_key("FOREIGN KEY(item_id) REFERENCES items(id)")
-        .to_string();
-    db.db.execute(&create_item_quantities, ());
-
-    Ok(())
+        .as_string();
+    let result = db.connection.execute(&create_items, ());
+    if let Err(e) = result {
+        panic!("DB Initialization error: {}", e);
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use tempfile;
     mod table_creation {
-
         use super::*;
-
         #[test]
-        fn item() {}
+        fn items() {
+            let dir = Path::new("/tmp/bartend.db");
+            let db = DB::new(dir);
+
+            create_tables(&db);
+
+            assert!(db.connection.table_exists(None, "items").unwrap());
+            assert!(db.connection.column_exists(None, "items", "id").unwrap());
+            assert!(db.connection.column_exists(None, "items", "name").unwrap());
+            assert!(
+                db.connection
+                    .column_exists(None, "items", "quantity")
+                    .unwrap()
+            );
+            let _ = std::fs::remove_file(dir);
+        }
     }
 }
