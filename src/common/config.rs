401e455effc572332f55f6b0fcc4f2bc5f9efeff
Bill Figliolini <BillFigliolini@gmail.com>
2026-03-06 17:16:59 -0500

    Implemented Display Units

--- src/common/config.rs
+++ src/common/config.rs
@@ -1,3 +1,24 @@
 //! # Config
 //! Provides program defaults. If a page or operation overrides them, they should not be modified.
-pub struct Config {}
+
+use std::fmt::Display;
+#[derive(Debug)]
+pub struct Config {
+    display_unit: DisplayUnit,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum DisplayUnit {
+    Metric,
+    Imperial,
+}
+
+impl Display for DisplayUnit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let text = match self {
+            DisplayUnit::Metric => "Metric (ml)",
+            DisplayUnit::Imperial => "Imperial (Oz)",
+        };
+        write!(f, "{text}")
+    }
+}
