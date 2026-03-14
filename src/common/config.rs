38cb36ce994ad199d6c42da67543046bdb55b900
Bill Figliolini <BillFigliolini@gmail.com>
2026-03-14 16:27:01 -0400

    Added Wrapper classes

--- src/common/config.rs
+++ src/common/config.rs
@@ -11,25 +11,34 @@ use crate::common::quantity::UnitSystem;
 //TODO:
 // Going to try a restructuring of this to see how well it works.
 // Unit System will be moved into Quantity, as that is where it belongs. X
-// Config will contain wrapper types around the relevant configurations.
+// Config will contain wrapper types around the relevant configurations. X
 // Each wrapper will allow for implementations in other modules as relevant.
 // The primary example I thinking of at present is implementing a viewable trait in
 // Presentation, to allow for separating out which data elements are responsible for which displays.
 
+#[derive(Debug, Clone, Copy)]
+pub struct DefaultUnitSystem(UnitSystem);
+
+#[derive(Debug, Clone)]
+pub struct DefaultDBPath(PathBuf);
+
 #[derive(Debug)]
 pub struct Config {
-    db_path: PathBuf,
-    default_unit_system: UnitSystem,
+    db_path: DefaultDBPath,
+    default_unit_system: DefaultUnitSystem,
 }
 impl Config {
     pub fn new() -> Self {
         let path = PathBuf::from("./bartend.db");
         Self {
-            db_path: path,
-            default_unit_system: UnitSystem::Metric,
+            db_path: DefaultDBPath(path),
+            default_unit_system: DefaultUnitSystem(UnitSystem::Metric),
         }
     }
     pub fn db_path(&self) -> &Path {
-        self.db_path.as_path()
+        self.db_path.0.as_path()
+    }
+    pub fn default_units(&self) -> UnitSystem {
+        self.default_unit_system.0
     }
 }
