1bb08e966b6a36918b1ed8fd041f3e5d2922bae9
Bill Figliolini <BillFigliolini@gmail.com>
2026-03-26 14:17:25 -0400

    Cleaned up code

--- src/common/config.rs
+++ src/common/config.rs
@@ -20,7 +20,7 @@ use crate::common::quantity::UnitSystem;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Config {
-    config_path: PathBuf,
+    path: PathBuf,
     db_path: PathBuf,
     default_unit_system: UnitSystem,
 }
@@ -40,103 +40,109 @@ const DEFAULT_CONFIG_FILE_NAME: &str = "Bartend.json";
 const DEFAULT_DB_NAME: &str = "Bartend.db";
 
 impl Config {
-    pub fn load(
-        config_path: Option<PathBuf>,
-        db_path: Option<PathBuf>,
-    ) -> Result<Config, ConfigError> {
-        let config_path = match config_path {
+    /// Loads a config if it exists at path, and creates one at path if it does not yet exist.
+    /// If path is None, will load/create a config file in the system config folder.
+    /// `db_path` provides an initial db location if config file does not exist.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if it can not read or write to the system config or data directories.
+    pub fn load(path: Option<PathBuf>, db_path: Option<PathBuf>) -> Result<Self, ConfigError> {
+        let config_path = match path {
             Some(path) => path,
-            None => match build_config_path() {
-                Ok(path) => path,
-                Err(e) => return Err(e),
-            },
+            None => build_config_path()?,
         };
-        let config = match config_path.exists() {
-            true => {
-                let file = match File::open(config_path) {
-                    Ok(file) => file,
-                    Err(_) => return Err(ConfigError::UnableToAccessConfigFile),
-                };
-                let reader = BufReader::new(file);
-                let deserialized_config = serde_json::from_reader(reader);
-                match deserialized_config {
-                    Ok(config) => config,
-                    Err(_) => return Err(ConfigError::ReadError),
-                }
+        let config = if config_path.exists() {
+            let Ok(file) = File::open(config_path) else {
+                return Err(ConfigError::UnableToAccessConfigFile);
+            };
+            let reader = BufReader::new(file);
+            match serde_json::from_reader(reader) {
+                Ok(config) => config,
+                Err(_) => return Err(ConfigError::ReadError),
             }
-            false => {
-                let db_path = match db_path {
-                    Some(path) => path,
-                    None => match build_db_path() {
-                        Ok(path) => path,
-                        Err(e) => return Err(e),
-                    },
-                };
+        } else {
+            let db_path = match db_path {
+                Some(path) => path,
+                None => build_db_path()?,
+            };
 
-                let config = Config {
-                    config_path,
-                    db_path,
-                    default_unit_system: UnitSystem::Metric,
-                };
+            let config = Self {
+                path: config_path,
+                db_path,
+                default_unit_system: UnitSystem::Metric,
+            };
 
-                if let Err(_) = config.save() {
-                    return Err(ConfigError::WriteError);
-                }
-                config
+            if let Err(_) = config.save() {
+                return Err(ConfigError::WriteError);
             }
+            config
         };
         Ok(config)
     }
 
+    /// Saves [`Config`] to disk at Config.path.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if it can not write to config.path or if the json file is malformed.
     pub fn save(&self) -> Result<(), ConfigError> {
         let file_result = OpenOptions::new()
             .write(true)
             .create(true)
             .truncate(true)
-            .open(&self.config_path);
-        let file = match file_result {
-            Ok(f) => f,
-            Err(_) => return Err(ConfigError::WriteError),
+            .open(&self.path);
+        let Ok(file) = file_result else {
+            return Err(ConfigError::WriteError);
         };
         let writer = BufWriter::new(file);
         match serde_json::to_writer(writer, &self) {
-            Ok(_) => Ok(()),
+            Ok(()) => Ok(()),
             Err(_) => Err(ConfigError::WriteError),
         }
     }
-
-    pub fn db_path(&self) -> &PathBuf {
+    #[must_use]
+    pub const fn db_path(&self) -> &PathBuf {
         &self.db_path
     }
-    pub fn default_units(&self) -> UnitSystem {
+    #[must_use]
+    pub const fn default_units(&self) -> UnitSystem {
         self.default_unit_system
     }
 }
 
+/// Builds the default config file path
+///
+/// # Errors
+///
+/// This function will return an error if unable to write to system config directory.
 fn build_config_path() -> Result<PathBuf, ConfigError> {
-    let config_dir = match dirs::config_dir() {
-        Some(dir) => dir,
-        None => return Err(ConfigError::UnableToAccessSystemConfigDir),
+    let Some(config_dir) = dirs::config_dir() else {
+        return Err(ConfigError::UnableToAccessSystemConfigDir);
     };
     let config_dir = config_dir.join(CONFIG_DIR_NAME);
-    if !config_dir.exists() {
-        if let Err(_) = fs::create_dir(&config_dir) {
-            return Err(ConfigError::UnableToCreateConfigFile);
-        }
+    if !config_dir.exists()
+        && let Err(_) = fs::create_dir(&config_dir)
+    {
+        return Err(ConfigError::UnableToCreateConfigFile);
     }
     Ok(config_dir.join(DEFAULT_CONFIG_FILE_NAME))
 }
 
+/// Builds the default db path
+///
+/// # Errors
+///
+/// This function will return an error if unable to write to system data directory.
 fn build_db_path() -> Result<PathBuf, ConfigError> {
-    let db_dir = match dirs::data_dir() {
-        Some(dir) => dir,
-        None => return Err(ConfigError::UnableToAccessSystemDataDir),
+    let Some(db_dir) = dirs::data_dir() else {
+        return Err(ConfigError::UnableToAccessSystemDataDir);
     };
     let db_dir = db_dir.join(CONFIG_DIR_NAME);
-    if !db_dir.exists() {
-        if let Err(_) = fs::create_dir(&db_dir) {
-            return Err(ConfigError::UnableToCreateDataDir);
-        };
+    if !db_dir.exists()
+        && let Err(_) = fs::create_dir(&db_dir)
+    {
+        return Err(ConfigError::UnableToCreateDataDir);
     }
     Ok(db_dir.join(DEFAULT_DB_NAME))
 }
@@ -166,7 +172,7 @@ mod tests {
             let test_file = test_dir.path().join("contents.json");
             let test_db = test_dir.path().join("contents.db");
             let config = Config {
-                config_path: test_file.clone(),
+                path: test_file.clone(),
                 db_path: test_db,
                 default_unit_system: UnitSystem::Metric,
             };
@@ -176,7 +182,7 @@ mod tests {
 
             let reader = BufReader::new(File::open(test_file).unwrap());
             let result_config: Config = serde_json::from_reader(reader).unwrap();
-            assert_eq!(config.config_path, result_config.config_path);
+            assert_eq!(config.path, result_config.path);
             assert_eq!(config.db_path, result_config.db_path);
         }
     }
