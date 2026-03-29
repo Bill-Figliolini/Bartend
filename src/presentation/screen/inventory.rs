088eb8b277d217a4f34b521ca69ff88278656370
Bill Figliolini <BillFigliolini@gmail.com>
2026-03-29 13:59:04 -0400

    Quantities are now cleared on save

--- src/presentation/screen/inventory.rs
+++ src/presentation/screen/inventory.rs
@@ -119,6 +119,7 @@ impl Inventory {
                 if self.errors.is_empty() {
                     let quantity = Quantity::new(quantity, self.input_unit);
                     let name = take(&mut self.input_name);
+                    self.input_quantity.clear();
                     match self.edit_state {
                         EditState::None => Some(application::Command::AddItem(name, quantity)),
                         EditState::Editing(item_id) => {
