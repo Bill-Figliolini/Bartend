39fb88b0b8a3dce8f71310015f2f04cacce0cd67
Bill Figliolini <BillFigliolini@gmail.com>
2026-03-31 16:55:45 -0400

    Reminder for later

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -1,5 +1,7 @@
 //! # Common
 //! Contains data structures and functions which are necessary throughout the stack.
+// TODO: Remove and move into logic
+
 pub mod category;
 pub mod config;
 pub mod item;
