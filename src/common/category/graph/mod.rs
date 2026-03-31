429e0f753e67f0db1c758f3d4eb2d832cc26a92c
Bill Figliolini <BillFigliolini@gmail.com>
2026-03-31 19:57:39 -0400

    Adjusted graph to be acyclic

--- src/common/category/graph/mod.rs
+++ src/common/category/graph/mod.rs
@@ -7,8 +7,8 @@ use std::{
     hash::Hash,
 };
 #[derive(Debug)]
-struct Graph<T: Copy + Eq + Hash> {
-    matrix: HashMap<T, HashSet<T>>,
+struct DirectedAcyclicGraph<T: Copy + Eq + Hash> {
+    graph: HashMap<T, HashSet<T>>,
 }
 #[derive(Debug)]
 enum GraphError {
@@ -16,10 +16,10 @@ enum GraphError {
     WouldIntroduceCycle,
 }
 
-impl<T: Copy + Eq + Hash> Graph<T> {
+impl<T: Copy + Eq + Hash> DirectedAcyclicGraph<T> {
     pub fn new() -> Self {
         Self {
-            matrix: HashMap::new(),
+            graph: HashMap::new(),
         }
     }
     pub fn build_from(vertices: &[T], edges: &[(T, T)]) -> Result<Self, GraphError> {
@@ -33,32 +33,51 @@ impl<T: Copy + Eq + Hash> Graph<T> {
         Ok(graph)
     }
     pub fn insert_vertex(&mut self, vertex: T) {
-        if !self.matrix.contains_key(&vertex) {
-            self.matrix.insert(vertex, HashSet::new());
+        if !self.graph.contains_key(&vertex) {
+            self.graph.insert(vertex, HashSet::new());
         }
     }
     fn contains_vertex(&self, vertex: &T) -> bool {
-        self.matrix.contains_key(vertex)
+        self.graph.contains_key(vertex)
     }
     pub fn insert_edge(&mut self, edge: &(T, T)) -> Result<(), GraphError> {
         if !(self.contains_vertex(&edge.0) && self.contains_vertex(&edge.1)) {
             return Err(GraphError::EdgeEndpointNotInGraph);
         }
-        self.matrix.get_mut(&edge.0).unwrap().insert(edge.1);
+        if self.implies(&edge.1, &edge.0) {
+            return Err(GraphError::WouldIntroduceCycle);
+        }
+        self.graph.get_mut(&edge.0).unwrap().insert(edge.1);
         Ok(())
     }
     fn get_edges(&self, vertex: &T) -> &HashSet<T> {
-        self.matrix.get(vertex).unwrap()
-    }
-    fn does_x_imply_y(&self, vertex_x: T, vertex_y: T) -> bool {
-        self.matrix.get(&vertex_x).unwrap().contains(&vertex_y)
+        self.graph.get(vertex).unwrap()
     }
     pub fn remove(&mut self, vertex: T) {
-        if !self.contains_vertex(&vertex) {
+        let Some(child_verticies) = self.graph.remove(&vertex) else {
             return;
+        };
+        for (_, adj_set) in self.graph.iter_mut() {
+            if adj_set.remove(&vertex) {
+                adj_set.extend(child_verticies.clone());
+            }
         }
     }
-    pub fn implies(&self, vertex: &T) -> HashSet<T> {
+    fn implies(&self, start_vertex: &T, end_vertex: &T) -> bool {
+        let current_set = self.graph.get(start_vertex);
+        match current_set {
+            Some(next_set) => {
+                for vertex in next_set {
+                    if vertex == end_vertex || self.implies(vertex, end_vertex) {
+                        return true;
+                    }
+                }
+                false
+            }
+            None => false,
+        }
+    }
+    pub fn implies_set(&self, vertex: &T) -> HashSet<T> {
         let mut implications = HashSet::new();
         let mut stack = Vec::new();
         if !self.contains_vertex(vertex) {
@@ -83,7 +102,7 @@ mod tests {
     use super::*;
     #[test]
     fn removing_results_in_lower_members_being_moved() {
-        let mut graph = Graph::build_from(&[1, 2, 3], &[(1, 2), (2, 3)]).unwrap();
+        let mut graph = DirectedAcyclicGraph::build_from(&[1, 2, 3], &[(1, 2), (2, 3)]).unwrap();
 
         graph.remove(2);
 
@@ -92,11 +111,21 @@ mod tests {
     }
     #[test]
     fn implication_results_in_indirect_child_nodes_returned() {
-        let graph = Graph::build_from(&[1, 2, 3, 4, 5], &[(1, 2), (2, 3), (3, 4), (4, 5)]).unwrap();
+        let graph =
+            DirectedAcyclicGraph::build_from(&[1, 2, 3, 4, 5], &[(1, 2), (2, 3), (3, 4), (4, 5)])
+                .unwrap();
 
-        let implication = graph.implies(&2);
+        let implication = graph.implies_set(&2);
 
         let expected_implication = HashSet::from([3, 4, 5]);
         assert_eq!(implication, expected_implication);
+    }
+    #[test]
+    fn cycles_not_allowed_at_insertion() {
+        let mut graph = DirectedAcyclicGraph::build_from(&[1, 2, 3], &[(1, 2), (2, 3)]).unwrap();
+
+        let insert_result = graph.insert_edge(&(3, 1));
+
+        assert!(insert_result.is_err())
     }
 }
