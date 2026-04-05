3b0ba012a38bb868eb7fee401a4dd95a01e019fe
Bill Figliolini <BillFigliolini@gmail.com>
2026-04-05 10:56:52 -0400

    Added derives for Category and CategoryID

--- src/common/category/mod.rs
+++ src/common/category/mod.rs
@@ -1,6 +1,8 @@
 mod graph;
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct CategoryID(pub i64);
+#[derive(Debug, Clone)]
 pub struct Category {
     id: CategoryID,
     name: String,
