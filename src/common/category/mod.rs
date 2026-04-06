4afd2e0ad2df152d95d912d90cd3003d47f9b4fe
Bill Figliolini <BillFigliolini@gmail.com>
2026-04-06 16:40:57 -0400

    Display and partialeq implementations

--- src/common/category/mod.rs
+++ src/common/category/mod.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 mod graph;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -6,4 +8,16 @@ pub struct CategoryID(pub i64);
 pub struct Category {
     id: CategoryID,
     name: String,
+}
+
+impl Display for Category {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+impl PartialEq for Category {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
 }
