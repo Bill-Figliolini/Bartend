ddfd17383bb242c55a18c4054b961b4dd0a1e47c
Bill Figliolini <BillFigliolini@gmail.com>
2026-04-23 16:55:51 -0400

    Removed MockDB as it will not be needed

--- src/persistence/mod.rs
+++ src/persistence/mod.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, sync::atomic::AtomicI64};
+use std::path::Path;
 
 use rusqlite::Connection;
 
@@ -8,22 +8,9 @@ pub mod sqlite;
 pub struct Database {
     pub connection: Connection,
 }
-
-#[derive(Debug)]
-pub struct MockDB {
-    current_id: AtomicI64,
-}
 pub trait Repository {
     fn execute(&mut self, sql: &str);
     fn get_last_id(&self) -> i64;
-}
-
-pub trait DBCreate {
-    fn create(db: &Database);
-}
-pub trait DBUnit {
-    fn update(self, db: &Database);
-    fn delete(self, db: &Database);
 }
 impl Database {
     pub fn new(path: impl AsRef<Path>) -> Self {
@@ -47,24 +34,5 @@ impl Repository for Database {
 
     fn get_last_id(&self) -> i64 {
         self.connection.last_insert_rowid()
-    }
-}
-
-impl MockDB {
-    pub fn new() -> Self {
-        Self {
-            current_id: AtomicI64::new(0),
-        }
-    }
-}
-
-impl Repository for MockDB {
-    fn execute(&mut self, _sql: &str) {
-        return;
-    }
-
-    fn get_last_id(&self) -> i64 {
-        self.current_id
-            .fetch_add(1, std::sync::atomic::Ordering::Relaxed)
     }
 }
