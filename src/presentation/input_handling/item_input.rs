0c7fb62e45144c0808aac15bc8d48e3afe9288a5
Bill Figliolini <BillFigliolini@gmail.com>
2026-05-17 15:09:13 -0400

    Implemented Item Input

--- src/presentation/input_handling/item_input.rs
+++ src/presentation/input_handling/item_input.rs
@@ -1,10 +1,18 @@
+use std::collections::HashSet;
+
 use crate::{
-    logic::{category::Category, config::Config, item::ItemBody, quantity::Unit},
+    logic::{
+        category::Category,
+        config::Config,
+        item::ItemBody,
+        quantity::{Quantity, Unit, UnitSystem},
+    },
     presentation::{
         Viewable,
         application::Message,
         input_handling::{InputCollection, InputMessage},
         widget::input::{
+            Error, Input, InputContents, InputOptionalContents,
             pick_input::{OptionalPickInput, RequiredPickInput},
             text_input::{number_input::NumberInput, string_input::StringInput},
         },
@@ -17,20 +25,112 @@ pub struct ItemInput {
     quantity_input: NumberInput<Message>,
     unit_input: RequiredPickInput<Unit, Message>,
     category_input: OptionalPickInput<Category, Message>,
+
+    errors: HashSet<Error>,
 }
 impl Viewable<Message> for ItemInput {
     fn view(&self) -> iced::Element<'_, Message> {
         todo!()
     }
 }
-impl InputCollection<ItemBody> for ItemInput {
-    fn new(config: &Config, msg: fn(InputMessage) -> Message) -> Self {
-        todo!()
+impl ItemInput {
+    pub fn new(
+        config: &Config,
+        msg: fn(InputMessage) -> Message,
+        categories: Vec<Category>,
+    ) -> Self {
+        let name_input = StringInput::new(
+            move |id, str| msg(InputMessage::String(id, str)),
+            "Name".to_string(),
+            String::new(),
+        );
+        let quantity_input = NumberInput::new(
+            move |id, str| msg(InputMessage::String(id, str)),
+            "Name".to_string(),
+            String::new(),
+        );
+        let units = match config.default_units() {
+            UnitSystem::Metric => Unit::Milliliter,
+            UnitSystem::Imperial => Unit::FluidOunce,
+        };
+        let unit_input = RequiredPickInput::new(
+            move |id, unit| msg(InputMessage::Unit(id, unit)),
+            Unit::get_units(),
+            Some(units),
+        );
+        let category_input = OptionalPickInput::new(
+            move |id, category| msg(InputMessage::Category(id, category)),
+            categories,
+            None,
+        );
+        Self {
+            name_input,
+            quantity_input,
+            unit_input,
+            category_input,
+            errors: HashSet::new(),
+        }
     }
+    fn clear(&mut self) {
+        self.name_input.clear();
+        self.quantity_input.clear();
+        self.category_input.clear();
+    }
+}
+
+impl InputCollection<(ItemBody, Option<Category>)> for ItemInput {
     fn update(&mut self, msg: InputMessage) {
-        todo!()
+        match msg {
+            InputMessage::String(id, str) if &id == self.name_input.id() => {
+                self.name_input.update(str)
+            }
+            InputMessage::String(id, str) if &id == self.quantity_input.id() => {
+                self.quantity_input.update(str);
+            }
+            InputMessage::Unit(id, unit) if &id == self.unit_input.id() => {
+                self.unit_input.update(unit);
+            }
+            InputMessage::Category(id, category) if &id == self.category_input.id() => {
+                self.category_input.update(category);
+            }
+            _ => unreachable!("Item Input has recieved invalid mesage {msg:?}"),
+        }
     }
-    fn output(&mut self) -> Result<ItemBody, ()> {
-        Ok(todo!())
+    fn output(&mut self) -> Result<(ItemBody, Option<Category>), ()> {
+        let name_result = self.name_input.get_output();
+        if let Err(ref e) = name_result {
+            self.errors.insert(e.clone());
+        }
+        let quantity_result = self.quantity_input.get_output();
+        if let Err(ref e) = quantity_result {
+            self.errors.insert(e.clone());
+        }
+        let units = self
+            .unit_input
+            .get_output()
+            .expect("Unit Input starts initialized, and is therefore infalible here");
+        let category = self
+            .category_input
+            .get_output()
+            .expect("Optional Pickers are infalible");
+        if self.errors.is_empty() {
+            let name = name_result.unwrap();
+            let quantity = Quantity::new(quantity_result.unwrap(), units);
+            Ok((ItemBody { name, quantity }, category))
+        } else {
+            Err(())
+        }
+    }
+
+    fn begin_edit(&mut self, edit: (ItemBody, Option<Category>), unit_system: UnitSystem) {
+        self.clear();
+        if let Some(category) = edit.1 {
+            self.category_input.update(category);
+        }
+        self.name_input.update(edit.0.name);
+        let quantity = edit.0.quantity.value(unit_system);
+        let units = edit.0.quantity.unit(unit_system);
+        self.quantity_input.update(quantity.to_string());
+        self.unit_input.update(units);
     }
 }
