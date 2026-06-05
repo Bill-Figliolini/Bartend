19ccbbc81babc58d9b9a4c3d8eee7891aca25723
Bill Figliolini <BillFigliolini@gmail.com>
2026-06-05 17:55:43 -0400

    Service input interface

--- src/presentation/input_handling/service_input.rs
+++ src/presentation/input_handling/service_input.rs
@@ -1,7 +1,7 @@
 use crate::{
     application,
-    models::{Category, Item, ItemID, Recipe},
-    presentation::widget::input::RequiredPickInput,
+    models::{Category, Item, ItemID, Quantity, Recipe},
+    presentation::{Viewable, input_handling::InputCollection, widget::input::RequiredPickInput},
 };
 
 struct ServiceInput {
@@ -10,4 +10,50 @@ struct ServiceInput {
 }
 struct IngredientUseInput {
     category: Category,
+    quantity: Quantity,
+    ingredients: Vec<RequiredPickInput<Item, application::Message>>,
+}
+impl Viewable<application::Message> for IngredientUseInput {
+    fn view(&self) -> iced::Element<'_, application::Message> {
+        todo!()
+    }
+}
+impl Viewable<application::Message> for ServiceInput {
+    fn view(&self) -> iced::Element<'_, application::Message> {
+        todo!()
+    }
+}
+impl InputCollection for IngredientUseInput {
+    fn update(&mut self, msg: super::InputMessage) {
+        todo!()
+    }
+
+    fn output(&mut self) -> Result<T, ()> {
+        todo!()
+    }
+
+    fn begin_edit(&mut self, edit: &T, unit_system: crate::models::UnitSystem) {
+        todo!()
+    }
+
+    fn clear(&mut self) {
+        todo!()
+    }
+}
+impl InputCollection for ServiceInput {
+    fn update(&mut self, msg: super::InputMessage) {
+        todo!()
+    }
+
+    fn output(&mut self) -> Result<T, ()> {
+        todo!()
+    }
+
+    fn begin_edit(&mut self, edit: &T, unit_system: crate::models::UnitSystem) {
+        todo!()
+    }
+
+    fn clear(&mut self) {
+        todo!()
+    }
 }
