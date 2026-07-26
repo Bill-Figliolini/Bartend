c03073e2a6cbeadaac8b75d6af1a0c30fe058b55
Bill Figliolini <BillFigliolini@gmail.com>
2026-07-26 17:23:24 -0400

    Interface

--- src/presentation/widget/input/multi_pick_input.rs
+++ src/presentation/widget/input/multi_pick_input.rs
@@ -1,7 +1,46 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, rc::Rc};
+
+use iced::{Element, widget::Id};
+
+use crate::presentation::application::Command;
 
 // could I turn the structure  {id, body} into a trait, to make things smoother here?
-struct MultipickInput<T> {
+struct MultipickInput<T, Message>
+where
+    T: Clone,
+    Message: Clone,
+{
+    id: Id,
     choices: Vec<T>,
     selected: HashSet<T>,
+    message: Rc<dyn Fn(Id, T) -> Message>,
+}
+
+impl<T, Message> MultipickInput<T, Message>
+where
+    T: Clone,
+    Message: Clone,
+{
+    pub fn new(
+        choices: Vec<T>,
+        already_selected: HashSet<T>,
+        message: Rc<dyn Fn(Id, T) -> Message>,
+    ) -> Self {
+        Self {
+            id: Id::unique(),
+            choices,
+            selected: already_selected,
+            message,
+        }
+    }
+    pub fn output(&mut self) -> HashSet<T> {
+        self.selected.clone()
+    }
+
+    pub fn view(&self) -> Element<'_, Message> {
+        todo!()
+    }
+    pub fn update(&mut self, message: Message) -> Option<Command> {
+        todo!()
+    }
 }
