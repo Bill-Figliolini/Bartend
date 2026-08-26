97c0c3fb23730740598b71a19f10206f331f3777
Bill Figliolini <BillFigliolini@gmail.com>
2026-08-25 21:54:27 -0400

    Error implementation

--- src/models/bartend_error.rs
+++ src/models/bartend_error.rs
@@ -30,24 +30,17 @@ impl Display for BartendError {
 }
 impl Display for LogicError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "LOGIC ERROR: ");
+        write!(f, "LOGIC ERROR: ")?;
         match self {
-            LogicError::InvalidCategoryRelation { parent, child } => write!(f, "Category Edge {parent} to {child} would create loop!"),
+            LogicError::InvalidCategoryRelation { parent, child } => {
+                write!(f, "Category Edge {parent} to {child} would create loop!")
+            }
             LogicError::InvalidCategory(category_id) => write!(f, "invalid Category {category_id}"),
             LogicError::InvalidItem(item_id) => write!(f, "invalid Item {item_id}"),
             LogicError::InvalidRecipe(recipe_id) => write!(f, "invalid Recipe {recipe_id}"),
             LogicError::CategoryNotInGraph(category_id) => {
                 write!(f, "Category not in graph {category_id}")
             }
-        }
-    }
-}
-impl Display for DBError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "DB ERROR: ");
-        match self {
-            DBError::RestrictViolation => write!(f, "Unable to delete restricted DB Row"),
-            DBError::External(error) => write!(f, "{error}"),
         }
     }
 }
